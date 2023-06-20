6a24092a02133ba0aa0352c571f40b465d1e7a0f
1ups_ <61560124+1upses@users.noreply.github.com>
2023-06-20 14:42:09 +0200

    few changes in original prompt and now prints out user input and assistant output to the console

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def generate_input() -> str:
     return audio_to_text("input.wav")
 
 if __name__ == "__main__":
-    messages = [{"role": "system", "content": "Un assistant vocal à l'image de siri ou google assistant, il est créatif, intelligent, et amical, ses réponses sont courtes et vont droit au but"}]
+    messages = [{"role": "system", "content": "Une assistante vocal à l'image de siri ou google assistant, elle est créative, intelligente, et amicale, ses réponses sont courtes et vont droit au but. Cette assistante doit se rappeller de sa nature d'assistante vocale, et devra répondre de la sorte; si elle veut dire \"2^3\" par exemple, elle dira plutôt \"deux puissance trois\"."}]
     while True:
-        messages.append({"role": "user", "content": generate_input()})
+        user_input:str = generate_input()
+        print(f"entrée utilisateur: {user_input}\n")
+        messages.append({"role": "user", "content": user_input})
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages)
         reply = response["choices"][0]["message"]["content"]
+        print(f"réponse de my_siri: {reply}\n")
         messages.append({"role": "system", "content": reply})
         text_to_speech(reply)
